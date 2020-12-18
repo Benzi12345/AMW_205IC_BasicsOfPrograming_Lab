7a5b74500ba27c4deb01af7c2d58a4d5ac033a54
Tymoteusz Hinca <hinca.tymek1@gmail.com>
2020-12-18 13:52:45 +0100

    Moja druga wersja kalkulatora. Tymoteusz Hinca

--- main.c
+++ main.c
@@ -8,21 +8,21 @@ int main() {
     double b;
     char z;
 
-    printf("Podaj pierwsza liczbe: \n");
+
     scanf("%lf", &a);
-    printf("Jakie dzialanie chcesz wykonac: \n");
+
     scanf(" %c", &z);
-    printf("Podaj druga liczbe: \n");
+
     scanf("%lf", &b);
 
     if(z == '+'){
-        printf("%f", a + b);
+        printf("%f", a+b);
     } else if(z == '-'){
-        printf("%f", a - b);
+        printf("%f", a-b);
     }else if(z == '/'){
-        printf("%f", a / b);
+        printf("%f", a/b);
     }else if(z == '*') {
-        printf("%f", a * b);
+        printf("%f", a*b);
     } else {
         printf("Zly operator");
     }
