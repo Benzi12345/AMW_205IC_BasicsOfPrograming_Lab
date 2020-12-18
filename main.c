92f93ab98e1f6ca734dfd6344345537438f832df
WeronikaPaszkowska <v.paszkowska@wp.pl>
2020-12-18 11:00:50 +0100

    Kalkulator dodawanie

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+    int main() {
+
+        printf("Podaj pierwsza liczbe\n");
+        int liczba1;
+        printf("Podaj druga liczbe\n");
+        int liczba2;
+
+
+        scanf("%d",&liczba1);
+
+
+        scanf("%d",&liczba2);
+
+
+        printf("Wynik to: %d\n",liczba1+liczba2);
+
+        return 0;
+    }
+}
