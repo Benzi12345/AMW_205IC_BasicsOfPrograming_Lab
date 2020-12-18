9fa1115d4f5ef629241c644c1274e6e7a03c7e64
Maciek Kostecki <kosteckimaciej@outlook.com>
2020-12-18 11:36:07 +0100

    Kalkulator prosty

--- main.c
+++ main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 int main() {
-    char operator;
-    int pie, dru;
-    printf("Wpisz operator (+, -, *,): ");
-    scanf("%c", &operator);
-    printf("Wpisz dwie liczby: ");
-    scanf("%i %i", &pie, &dru);
+char operator;
+int pie, dru;
+printf("Wpisz operator (+, -, *,): ");
+scanf("%c", &operator);
+printf("Wpisz dwie liczby: ");
+scanf("%i %i", &pie, &dru);
 
-    switch (operator) {
-        case '+':
-            printf("%.1i + %.1i= %.1i", pie, dru, pie + dru);
-            break;
-        case '-':
-            printf("%.1i - %.1i = %.1i", pie, dru, pie - dru);
-            break;
-        case '*':
-            printf("%.1i * %.1i = %.1i", pie, dru, pie * dru);
-            break;
-        case '/':
-            printf("%.1i / %.1i = %.1i", pie, dru, pie / dru);
-            break;
-        default:
-            printf("Blad, operator znajduje sie w zlym miejscu");
-    }
+switch (operator) {
+case '+':
+printf("%.1i + %.1i= %.1i", pie, dru, pie + dru);
+break;
+case '-':
+printf("%.1i - %.1i = %.1i", pie, dru, pie - dru);
+break;
+case '*':
+printf("%.1i * %.1i = %.1i", pie, dru, pie * dru);
+break;
+case '/':
+printf("%.1i / %.1i = %.1i", pie, dru, pie / dru);
+break;
+default:
+printf("Blad, operator znajduje sie w zlym miejscu");
+}
 
-    return 0;
+return 0;
 }
