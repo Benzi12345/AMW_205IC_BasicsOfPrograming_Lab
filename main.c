20ea9fa798cb5b9d3e238fde546e89de24245423
Dawid120b <41784769+Dawid120b@users.noreply.github.com>
2020-12-18 16:47:27 +0100

    Mój pierwszy kalkulator

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello,Dawid Andrzejczak!\n");
+int main ()
+{
+    int a, b, i;
+    char c;
+    printf("Wpisz pierwsza liczbe: \n");       //Podaj pierwsza liczbę
+    scanf("%d", &a);
+    printf("Wpisz druga liczbe: \n");          //Podaj drugą liczbę
+    scanf("%d", &b);
+    getchar();
+    printf("Podaj znak: \n");    //Podaj znak (+ , - , * lub /)
+    scanf("%c", &c);
+
+    switch (c)   // działanie
+    {
+        case   '+': i=a+b;                      // Dodawanie
+            printf("Wynik to: %d\n", i);  // Wynik dodawania
+            break;
+        case   '-': i=a-b;                      // Odejmowanie
+            printf("Wynik to: %d\n", i);  //Wynik odejmowania
+            break;
+        case   '*': i=a*b;                      // Mnożenie
+            printf("Wynik to: %d\n", i);  // Wynik mnożenia
+            break;
+        case   '/': i=a/b;                      // Dzielenie
+            printf("Wynik to: %d\n", i);  //Wynik dzielenia
+
+            if (b==0)         //Warunek
+            {
+                printf ("Dzielenie przez zero jest niemozliwe"); //Komunikat
+            }
+
+            break;
+
+    }
     return 0;
 }
