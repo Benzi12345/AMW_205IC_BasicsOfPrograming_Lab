193352e4ed607b711f96f69485fd3124c1cc5ce8
Agata Grzelczak <ag.grzelczak@gmail.com>
2020-12-18 14:51:18 +0100

    Kalkulator
    Jeśli mogę, chciałabym poprosić Pana o pomoc. Nie ważne jaką wartość przypiszę do zmiennych l1 i l2,  pętla nie kończy się. Nie mogę się jednak dopatrzeć błędu. Mógłby mi Pan go wskazać?

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("Hello, Grzelczak Agata!\n");
+    float l1;
+    float l2;
+    char znak;
+    float wynik;
+
+    do {
+        printf("\nPodaj dzialanie\n");
+        scanf("\n%f %c %f", &l1, &znak, &l2);
+    }while (_isnanf(l1||l2));
+
+
+    switch(znak){
+        case '+': wynik = l1 + l2; break;
+        case '-': wynik = l1 - l2; break;
+        case '*': wynik = l1 * l2; break;
+        case '/': wynik = l1 / l2; break;
+    }
+
+    printf("\nWynik Twojego dzialania: \n%g \n\n", wynik);
+
     return 0;
 }
