3dc101cbb0c871529042db122b44e81562418eb2
rafalkaminski111 <legustogaming@gmail.com>
2020-12-18 18:54:08 +0100

    Kalkulator V2 działa na dowolną ilość liczb 0-9 nie wiem jak wyciągnąć ze stringa liczby wielo cyfrowe

--- main.c
+++ main.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 
 int main() {
-    float wynik = 0, l1 = 0, l2 = 0;
-    char opr;
-    printf("Podaj pierwsza liczbe\n");
-    scanf(" %f", &l1);
-    printf("Podaj druga liczbe\n");
-    scanf(" %f", &l2);
-    printf("Podaj operacje +,-,*,/\n");
-    scanf(" %c", &opr);
-    switch (opr) {
-        case '+':
-            wynik = l1 + l2;
-            break;
-        case '-':
-            wynik = l1 - l2;
-            break;
-        case '*':
-            wynik = l1 * l2;
-            break;
-        case '/':
-            if (l2 != 0)
-                wynik = l1 / l2;
+    char d[100];
+    int x=0;
+    printf("Podaj dzialanie, znaki rozdzielaj spacjami\n");
+    gets(d);
+    int len=strlen(d);
+    float wynik=d[0]-48;
+    float l2=0;
+    for(int i=2; i<len; i++) {
+        if (i % 2 == 0)
+            if (isdigit(d[i]) == 0)
+                x = i;
             else {
-                printf("Nie dziel przez zero");
-                return 0;
+                l2 = d[i]-48;
+                switch (d[x]) {
+                    case '+':
+                        wynik = wynik + l2;
+                        break;
+                    case '-':
+                        wynik = wynik - l2;
+                        break;
+                    case '*':
+                        wynik = wynik * l2;
+                        break;
+                    case '/':
+                        if (l2 != 0)
+                            wynik = wynik / l2;
+                        else {
+                            printf("Nie dziel przez zero");
+                            return 0;
+                        }
+                        break;
+                    default:
+                        printf("Zly znak");
+                        return 0;
+                }
+
             }
-            break;
-        default:
-            printf("Zly znak");
-            return 0;
+
+
     }
-    printf("Wynik = %f \n", wynik);
-    return 0;
+    printf(" %f", wynik);
 }
