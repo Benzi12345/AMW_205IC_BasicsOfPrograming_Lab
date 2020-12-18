df44afbf00fdc6cd047abf90ef8947621ae0b926
WeronikaPaszkowska <v.paszkowska@wp.pl>
2020-12-18 11:44:28 +0100

    Kalkulator prosty, wersja poprawiona

--- main.c
+++ main.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
     int main() {
-
+        printf("Wybierz dzialanie:\n1.dodawanie\n2.odejmowanie\n3.mnozenie\n4.dzielenie \n");
+        int wybor;
         printf("Podaj pierwsza liczbe\n");
         int liczba1;
         printf("Podaj druga liczbe\n");
         int liczba2;
 
 
-        scanf("%d",&liczba1);
+        scanf("%d %i %i", &wybor,&liczba1,&liczba2);
 
 
-        scanf("%d",&liczba2);
 
-
-        printf("Wynik to: %d\n",liczba1+liczba2);
+        if(wybor == 1) {
+            printf("Wynik to: %d\n", liczba1+liczba2);
+        }
+        else if(wybor ==2 ) {
+            printf("Wynik to: %d\n", liczba1-liczba2);
+        }
+        else if(wybor ==3 ) {
+            printf("Wynik to: %d\n",  liczba1*liczba2);
+        }
+        else if(wybor ==4 ) {
+            printf("Wynik to: %d\n",liczba1*liczba2);
+        }
 
         return 0;
     }
-}
