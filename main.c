99ef5f1532705d3d89524a53a84c0c60b775f2ea
Bodziix <76045904+Bodziix@users.noreply.github.com>
2020-12-18 20:00:29 +0100

    Kalkulator trzeci update
    poprawiono:
    wynik dzielenia jest teraz wyświetlany poprawnie z wynikiem po przecinku
    zmieniono:
    sposób wprowadzania został ulepszony teraz można napisać swoje działanie w logiczny sposób np. 2+2, 5-3 itd. a nie tak jak wcześniej - 5 2

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 int main() {
     char zn;
     int l1, l2, wynik=0;
+    double wynikdzielenia=0;
 
-    printf("Podaj znak (+, -, *, /) oraz dwie liczby:\n");
-    scanf("%c%i%i", &zn, &l1, &l2);
+    printf("Podaj swoje działanie\n");
+    scanf("%i%c%i", &l1, &zn, &l2);
 
     if(zn=='+'){
         wynik=l1+l2;
@@ -18,12 +19,13 @@ int main() {
             printf("nie po prostu nie...");
             return 0;
         }else {
-            wynik=l1/l2;
+            wynikdzielenia=(double)l1/(double)l2;
+            printf("Wynik %i %c %i = %lf", l1, zn, l2, wynikdzielenia);
+            return 0;
         }
     }else {
         printf("coś poszło nie tak\n try again");
     }
     printf("Wynik %i %c %i = %i", l1, zn, l2, wynik);
-
     return 0;
 }
