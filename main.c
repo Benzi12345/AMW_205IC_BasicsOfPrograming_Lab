6f0a5cdc778276c3dc47e5768dfa93a3c0a4e23c
Kacper Czech <kacperczech13@gmail.com>
2020-12-18 10:16:05 +0100

    Prosty kalkulator.

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, Czech Kacper\n");
-    return 0;
+
+    int liczba1,liczba2,suma,roznica,iloraz,iloczyn;
+    char znak;
+
+    printf("Kalkulator");
+    printf("\nPodaj operator: (+,-,/,*): \n");
+    scanf("%c",&znak);
+    printf("\n Podaj pierwsza liczbe : ");
+    scanf("%i",&liczba1);
+    printf("\n Podaj druga liczbe : ");
+    scanf("%i",&liczba2);
+
+    printf("Wprowadzono liczby: %i,%i",liczba1,liczba2);
+
+    if(znak== '+' ){
+        suma=liczba1+liczba2;
+        printf("\n Wynik dodawania liczb %i + %i wynosi: %i",liczba1,liczba2,suma);
+    }
+    else if(znak== '-' ){
+        roznica=liczba1-liczba2;
+        printf("\n Wynik odejmowania liczb %i - %i wynosi: %i",liczba1,liczba2,roznica);
+    }
+    else if(znak== '*' ){
+        iloczyn=liczba1*liczba2;
+        printf("\n Wynik mnozenia liczb %i * %i wynosi: %i",liczba1,liczba2,iloczyn);
+    }
+    else if(znak== '/' ){
+        iloraz=liczba1/liczba2;
+        printf("\n Wynik dzielenia liczb %i / %i wynosi: %i",liczba1,liczba2,iloraz);
+    }
+
 }
+// Napisz prosty kalkulator
