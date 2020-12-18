4a86351ed34e4e8e75c05b6da5116e7fe79ffd04
Tomasz Januszewski <kemot11335@gmail.com>
2020-12-18 13:26:43 +0100

    Poprawiona wersja podstawowego kalkulatora

--- main.c
+++ main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    float l1, l2, wynik, wybor;
+    int l1,l2;
+    float wynik, wybor;
 
     printf("Podaj liczby: \n");
-    scanf("%f\n%f", &l1,&l2);
+    scanf("%i\n%i", &l1,&l2);
     printf("Wybierz dzialanie 1.Dodawanie, 2.Odejmowanie, 3.Dzielenie, 4.Mnozenie: ");
     scanf("%f", &wybor);
     if(wybor == 1)
     {
         wynik=l1+l2;
-        printf("Wynik dodawania: %f",wynik);
     }
     else if(wybor == 2)
     {
         wynik=l1-l2;
-        printf("Wynik odejmowania: %f",wynik);
     }
     else if(wybor == 3 &&  l2 == 0)
     {
@@ -24,16 +23,15 @@ int main() {
    else if(wybor == 3)
     {
         wynik=l1/l2;
-        printf("Wynik dzielenia: %f",wynik);
     }
     else if(wybor == 4)
     {
         wynik=l1*l2;
-        printf("Wynik mnozenia: %f",wynik);
     }
     else if(wybor > 4)
     {
         printf("Wprowadzono zla liczbe");
     }
+    printf("Wynik obliczen: %f",wynik);
     return 0;
 }
