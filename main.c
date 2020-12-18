97e4cea635374bdb00fced902f31f72e113e8126
Piotr Lonn <lonnekxd@gmail.com>
2020-12-18 13:18:37 +0100

    Poprawiony kalkulator niestety nie działa mi teraz że po dzieleniu przez 0 aplikacja odpowiada

--- main.c
+++ main.c
@@ -2,47 +2,42 @@
 
 int main()
 {
-    int a, b, choice;
+    int num1,num2;
+    float result;
+    char ch;
 
-    printf("Wybierz\n");
-    printf("1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n\n");
-    scanf("%d", &choice);
+    printf("Wpisz pierwsza liczbe: ");
+    scanf("%d",&num1);
+    printf("Wpisz druga liczbe: ");
+    scanf("%d",&num2);
 
-    if(choice > 4)
-    {
-        printf("Zly wybor\n");
-    }
-    else
-    {
-        printf("Wpisz dwie liczby\n");
-        scanf("%d %d", &a, &b);
-    }
+    printf("Wybierz operacje (+,-,*,/): ");
+    scanf(" %c",&ch);
 
+    result=0;
+    switch(ch)
+    {
+        case '+':
+            result=num1+num2;
+            break;
 
-    if(choice == 1)
-    {
-        printf("Wnyik dodawania liczb %d i %d jest rowny %d\n", a, b, (a+b));
-    }
-    else if(choice == 2)
-    {
-        printf("Wynik odejmowania liczb %d i %d jest rowny %d\n", a, b, (a-b));
-    }
-    else if(choice == 3)
-    {
-        printf("Wynik mnozenia liczb %d i %d jest rowny %d\n", a, b, (a*b));
-    }
-    else if(choice == 4)
-    {
-        if(b != 0)
-            printf("Wynik dzielenia liczb %d and %d jest rowny %d\n", a, b, (a/b));
-        else
-            printf("Nie mozna dzielic przez 0\n");
-    }
-    else
-    {
-        printf("\n""Wpisz dowolne z tych numerów 1, 2, 3, 4\n");
+        case '-':
+            result=num1-num2;
+            break;
+
+        case '*':
+            result=num1*num2;
+            break;
+
+        case '/':
+            result=(float)num1/(float)num2;
+            {
+                if(result==0)
+                    printf("Nie można dzielić przez 0");
+            }
+            break;
     }
 
-
+    printf("Wynik: %d %c %d = %f\n",num1,ch,num2,result);
     return 0;
 }
