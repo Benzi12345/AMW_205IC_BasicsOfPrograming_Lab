710c9c830a068ba1f633b7b752d324b40a0587e7
Mikolaj Iciak <mskowy@op.pl>
2020-12-18 13:47:57 +0100

    Druga wersja kalkulatora

--- main.c
+++ main.c
@@ -6,28 +6,26 @@ int main(){
     float wynik;
     char znak;
 
-    printf("Wpisz pierwsza liczbe: ");
+    printf("Wpisz dzialanie: ");
     scanf("%i", &a);
-    printf("Wpisz druga liczbe: ");
+    scanf("%c", &znak);
     scanf("%i", &b);
-    printf("Podaj znak operacji: ");
-    scanf(" %c", &znak);
 
     if(znak == '+'){
         wynik = a + b;
-        printf("Wynik dzialania to: %f", wynik);
+        printf("%f", wynik);
     }
     if(znak == '-'){
         wynik = a - b;
-        printf("Wynik dzialania to: %f", wynik);
+        printf("%f", wynik);
     }
     if(znak == '*'){
         wynik = a * b;
-        printf("Wynik dzialania to: %f", wynik);
+        printf("%f", wynik);
     }
     if(znak == '/'){
         wynik = a / b;
-        printf("Wynik dzialania to: %f", wynik);
+        printf("%f", wynik);
     }
 
 }
